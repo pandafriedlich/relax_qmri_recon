afeafa07f0968024bd2a5e0371dc4ba2fc3e729c
yidong_tud_linux <y.zhao-8@tudelft.nl>
2023-07-13 18:22:35 +0200

    Make configuration updateable from a dict

--- configuration/config.py
+++ configuration/config.py
@@ -3,11 +3,22 @@ import dataclasses
 import os
 
 
+class BasicConfig:
+    def update(self, new: dict):
+        """
+        Update the configuration from a dict.
+        :param new:
+        :return:
+        """
+        for key, value in new.items():
+            setattr(key, value)
+
+
 @dataclasses.dataclass
-class ReconstructionBackboneConfig:
+class ReconstructionBackboneConfig(BasicConfig):
     forward_operator: str = 'fft2'
     backward_operator: str = 'ifft2'
-    input_channels: int = 18
+    in_channels: int = 18
     num_steps: int = 4
     recurrent_hidden_channels: int = 64
     recurrent_num_layers: int = 4
@@ -21,7 +32,7 @@ class ReconstructionBackboneConfig:
 
 
 @dataclasses.dataclass
-class SensitivityRefinementModuleConfig:
+class SensitivityRefinementModuleConfig(BasicConfig):
     in_channels: int = 2
     out_channels: int = 2
     num_filters: int = 8
@@ -30,14 +41,15 @@ class SensitivityRefinementModuleConfig:
 
 
 @dataclasses.dataclass
-class DataSetConfiguration:
+class DataSetConfiguration(BasicConfig):
     acceleration_factors: typing.Tuple[int] = (4., 8., 10.)
     modality: str = 't1map'
     overwrite_split: bool = False
 
 
 @dataclasses.dataclass
-class TrainerConfig:
+class TrainerConfig(BasicConfig):
+    batch_size: int = 2
     dataloader_num_workers: int = 8
     weight_decay: float = 0.
     lr_decay: float = 0.9
@@ -46,7 +58,7 @@ class TrainerConfig:
     save_checkpoint_every: int = 10
     save_optimizer_factor: int = 2
     validation_every: int = 10
-    load_pretrained_weight: typing.Union[str, bytes, os.PathLike] = None
+    load_pretrained_weight: typing.Optional[typing.Union[str, bytes, os.PathLike]] = None
     load_pretrained_latest: bool = False
     combined_loss_weight: typing.Dict[str, float] = \
         dataclasses.field(default_factory=
@@ -54,7 +66,6 @@ class TrainerConfig:
 
 
 @dataclasses.dataclass
-class TrainingTrackingConfig:
-    save_training_loss_every: int = 10            # in steps not epochs
-    save_training_image_every: int = 500          # in steps not epochs
-
+class TrainingTrackingConfig(BasicConfig):
+    save_training_loss_every: int = 10  # in steps not epochs
+    save_training_image_every: int = 500  # in steps not epochs
