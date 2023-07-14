5a3c54a659a4fc1e214243e22db9aadec04395bc
yidong_tud_linux <y.zhao-8@tudelft.nl>
2023-07-14 23:10:02 +0200

    Fix configuration/config grammatic error.

--- configuration/config.py
+++ configuration/config.py
@@ -25,9 +25,9 @@ class ReconstructionBackboneConfig(BasicConfig):
     recurrent_num_layers: int = 4
     no_parameter_sharing: bool = True
     learned_initializer: bool = True
-    initializer_initialization: str = None,
-    initializer_channels: typing.Tuple[int, ...] = (32, 32, 64, 64),
-    initializer_dilations: typing.Tuple[int, ...] = (1, 1, 2, 4),
+    initializer_initialization: str = None
+    initializer_channels: typing.Tuple[int, ...] = (32, 32, 64, 64)
+    initializer_dilations: typing.Tuple[int, ...] = (1, 1, 2, 4)
     initializer_multiscale: int = 1
     normalized: bool = False
 
