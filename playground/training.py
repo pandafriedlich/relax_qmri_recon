aa3953fc9c632f6ff9cfe759d66d4ed7252a9185
yidong_tud_linux <y.zhao-8@tudelft.nl>
2023-07-15 08:36:06 +0200

    Add commandline interface support for training function.

--- playground/training.py
+++ playground/training.py
@@ -1,4 +1,5 @@
 import yaml
+import click
 from configuration.config import (TrainerConfig,
                                   TrainingTrackingConfig,
                                   ReconstructionBackboneConfig,
@@ -7,39 +8,62 @@ from configuration.config import (TrainerConfig,
 from trainer.qmri_recon_trainer import QuantitativeMRITrainer
 from data.paths import CMRxReconDatasetPath
 
-dataset_path_yaml = "../yamls/cmrxrecon_dataset.yaml"
-training_config_yaml = "../yamls/all_acc_t1_5_fold.yaml"
 
-# get datapath handler
-data_path_handler = CMRxReconDatasetPath(dataset_path_yaml)
+@click.command()
+@click.option('-s',
+              '--split',
+              default=0,
+              type=int,
+              help='Perform training on the given split, valid values are integers in [0, 5)')
+@click.option('-d',
+              '--dataset-config',
+              'dataset_config_path',
+              default='../yamls/cmrxrecon_dataset.yaml',
+              type=str,
+              help='Path to dataset configuration file.'
+              )
+@click.option('-r',
+              '--running-config',
+              'running_config_path',
+              required=True,
+              type=str,
+              help='Path to the running configuration file')
+def train(split, dataset_config_path, running_config_path):
+    """The main function for training with command line interface."""
+    # get datapath handler
+    data_path_handler = CMRxReconDatasetPath(dataset_config_path)
 
-# load configuration files
-with open(training_config_yaml) as stream:
-    training_configs = yaml.load(stream, yaml.Loader)
-recon_config = ReconstructionBackboneConfig().update(
-    training_configs['recon_backbone_config']
-)
-ser_config = SensitivityRefinementModuleConfig().update(
-    training_configs['sensitivity_refinement_config']
-)
-dataset_config = DataSetConfiguration().update(
-    training_configs['dataset_config']
-)
-trainer_config = TrainerConfig().update(
-    training_configs['trainer_config']
-)
-tracking_config = TrainingTrackingConfig().update(
-    training_configs['training_tracking_config']
-)
+    # load configuration files
+    with open(running_config_path) as stream:
+        training_configs = yaml.load(stream, yaml.Loader)
+    recon_config = ReconstructionBackboneConfig().update(
+        training_configs['recon_backbone_config']
+    )
+    ser_config = SensitivityRefinementModuleConfig().update(
+        training_configs['sensitivity_refinement_config']
+    )
+    dataset_config = DataSetConfiguration().update(
+        training_configs['dataset_config']
+    )
+    trainer_config = TrainerConfig().update(
+        training_configs['trainer_config']
+    )
+    tracking_config = TrainingTrackingConfig().update(
+        training_configs['training_tracking_config']
+    )
 
-trainer = QuantitativeMRITrainer(run_name=training_configs['run_name'],
-                                 path_handler=data_path_handler,
-                                 split=0,
-                                 disable_tracker=False,
-                                 recon_config=recon_config,
-                                 ser_config=ser_config,
-                                 data_set_config=dataset_config,
-                                 training_config=trainer_config,
-                                 tracker_config=tracking_config
-                                 )
-trainer.train()
+    trainer = QuantitativeMRITrainer(run_name=training_configs['run_name'],
+                                     path_handler=data_path_handler,
+                                     split=split,
+                                     disable_tracker=False,
+                                     recon_config=recon_config,
+                                     ser_config=ser_config,
+                                     data_set_config=dataset_config,
+                                     training_config=trainer_config,
+                                     tracker_config=tracking_config
+                                     )
+    trainer.train()
+
+
+if __name__ == '__main__':
+    train()
